c6b65289920bf2a79f86d8e6e11196b658488cb1
Hardik Thakur <66474341+hardik-thakurrr@users.noreply.github.com>
2021-01-14 15:30:01 +0530

    Created  Task1_ProductOfTwo.cpp
    The program Calculates product of two numbers

---
+++ Task1_ProductOfTwo.cpp
@@ -0,0 +1,16 @@
+//Write a C++ Program to find the product of two numbers entered by the User
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a,b,product;
+    cout<<"\n Enter the First Value -> ";
+    cin>>a;
+    cout<<"\n Enter the Second Value -> ";
+    cin>>b;
+    product=a*b;
+    cout<<"\n The Final Answer is -> "<<product<<endl;
+    return 0;
+}
