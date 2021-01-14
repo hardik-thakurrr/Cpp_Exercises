db080a19c68cb0db7de1b043f8863086f9bb59e9
Hardik Thakur <66474341+hardik-thakurrr@users.noreply.github.com>
2021-01-14 15:23:29 +0530

    Created Task1_Sizeof.cpp
    The program Calculates size of Datatypes

---
+++ Task1_Sizeof.cpp
@@ -0,0 +1,13 @@
+//Write a C++ Program to Find Size of int, float, double and char in Your System
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"\n The Size of char is -> "<<sizeof(char)<<endl;
+    cout<<"\n The Size of int is -> "<<sizeof(int)<<endl;
+    cout<<"\n The Size of int is -> "<<sizeof(float)<<endl;
+    cout<<"\n The Size of int is -> "<<sizeof(double)<<endl;
+    return 0;
+}
